4077fcd25a3a55d59c14b7bc3d081863165b41e1
peggyfisher <peg.fisher@comcast.net>
2025-01-22 14:12:58 -0800

    03_01b initial commit

---
+++ app/src/SampleUI.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class SampleUI {
+    private JPanel contentPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button1;
+    private JButton button2;
+    private JRadioButton tuesdayRadioButton;
+    private JRadioButton fridayRadioButton;
+    private JRadioButton mondayRadioButton;
+    private JRadioButton wednesdayRadioButton;
+    private JRadioButton thursdayRadioButton;
+    private JComboBox comboBox1;
+    private JCheckBox checkBox1;
+    private JCheckBox checkBox2;
+    private JCheckBox checkBox3;
+}
