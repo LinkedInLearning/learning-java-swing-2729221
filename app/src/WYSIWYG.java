56e7723eb9ee67d9ab0eeb6d7f8a5659fb723335
peggyfisher <peg.fisher@comcast.net>
2025-01-21 16:04:02 -0800

    01_02e initial commit

---
+++ app/src/WYSIWYG.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class WYSIWYG {
+    private JButton btnClickMe;
+    private JCheckBox checkBox1;
+    private JTextField textField1;
+    private JLabel lblTitle;
+}
